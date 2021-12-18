9dbdf2d9fb19ed1b94761fb1a0aa3beb49da3edb
varlese <1361760+varlese@users.noreply.github.com>
2021-12-18 15:59:55 -0500

    Set up API calls to test endpoints.

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const { fromJS } = require('immutable');
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
@@ -13,16 +14,45 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+//@todo move rover name into variable on API
 
-// example API call
-app.get('/apod', async (req, res) => {
+//Get Mars rover manifest data
+//@todo move API call for each call into a function to more easily parse data for later
+app.get('/rovers', async(req,rest) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
+    } catch(err) {
         console.log('error:', err);
     }
 })
+
+//Get Mars rover photos
+app.get('/mars-photos', async(req,rest) => {
+    try {
+        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+            console.log(roverData)
+    } catch(err) {
+        console.log('error:', err);
+    }
+})
+
+// example API response/log
+// app.get('/ping', async(req,res) => {
+//     console.log('pong')
+//     res.send('pong')
+// })
+
+// example API call
+// app.get('/apod', async (req, res) => {
+//     try {
+//         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+//             .then(res => res.json())
+//         res.send({ image })
+//     } catch (err) {
+//         console.log('error:', err);
+//     }
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
