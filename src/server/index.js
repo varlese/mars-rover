6bed28deda99c027d53a78bb6aa95272d7a87d28
varlese <1361760+varlese@users.noreply.github.com>
2021-12-19 16:03:15 -0500

    Added functionality to parse data from the rover manifest and rover photos returned from API.

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const { fromJS } = require('immutable');
+const { Map, List } = require('immutable');
+const dayjs = require('dayjs');
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
-const path = require('path')
+const path = require('path');
+const res = require('express/lib/response');
 
 const app = express()
 const port = 3000
@@ -17,22 +19,56 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 //@todo move rover name into variable on API
 
 //Get Mars rover manifest data
-//@todo move API call for each call into a function to more easily parse data for later
-app.get('/rovers', async(req,rest) => {
+app.get('/rovers', async(req,res) => {
     try {
         let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
+
+            .then(res => {
+                console.log(res)
+                return res
+            })
+
+            .then(res => res.photo_manifest)
+
+            .then(res => {
+                return {
+                    name: res.name,
+                    landing_date: dayjs(res.landing_date).format('MMMM D, YYYY'),
+                    launch_date: dayjs(res.launch_date).format('MMMM D, YYYY'),
+                    status: res.status,
+                    max_date: res.max_date,
+                }
+            })
+
+        res.send(roverData)
     } catch(err) {
         console.log('error:', err);
     }
 })
 
 //Get Mars rover photos
-app.get('/mars-photos', async(req,rest) => {
+app.get('/mars-photos', async(req,res) => {
     try {
-        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${process.env.API_KEY}`)
+        let roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-            console.log(roverData)
+
+            .then(res => {
+                console.log(res)
+                return res
+            })
+
+            .then(res => res.photos)
+
+            .then(res => {
+                const roverPhotos = List(res)
+                return roverPhotos.map((photo, key) => {
+                    console.log(key)
+                    return photo.img_src
+                })
+            })
+
+            res.send(roverPhotos)
     } catch(err) {
         console.log('error:', err);
     }
