45628e262961ba950641e5e84f46f68b6e9a6e73
varlese <1361760+varlese@users.noreply.github.com>
2022-01-14 21:07:52 -0500

    Added hamburger menu to mobile view. Set up ESLint to standardize code.

--- src/public/client.js
+++ src/public/client.js
@@ -1,16 +1,17 @@
-let {setState, getState, getStateAsMap} = (() => {
-    let store = Immutable.Map({
-        user: { name: "Student" },
+/* global dayjs, Immutable */
+let {setState, getState, getStateAsMap} = ( () => {
+    let store = Immutable.Map( {
+        user: { name: 'Student' },
         apod: '',
         rovers: ['Curiosity', 'Opportunity', 'Spirit'],
         roverManifestData: '',
         roverPhotos: null,
         roverName: null,
-        date: dayjs('2020-10-12').format('YYYY-MM-DD'),
+        date: dayjs( '2020-10-12' ).format( 'YYYY-MM-DD' ),
         perPage: 8,
         page: 0,
         pages: null,
-    })
+    } )
 
     const getStateAsMap = () => {
         return store
@@ -20,28 +21,28 @@ let {setState, getState, getStateAsMap} = (() => {
         return getStateAsMap().toObject()
     }
 
-    const setState = (newState) => {
-        store = store.merge(newState)
+    const setState = ( newState ) => {
+        store = store.merge( newState )
     }
 
-    setState({roverName: getState().rovers[0]})
+    setState( {roverName: getState().rovers[0]} )
 
     return {
         setState: setState,
         getState: getState,
         getStateAsMap: getStateAsMap,
     }
-})()
+} )()
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById( 'root' )
 
-const updateStore = (newState) => {
-    setState(newState)
-    render(root, getState())
+const updateStore = ( newState ) => {
+    setState( newState )
+    render( root, getState() )
 }
 
-const render = async (root) => {
+const render = async ( root ) => {
     root.innerHTML = App()
 }
 
@@ -49,68 +50,71 @@ const render = async (root) => {
 const App = () => {
     let { rovers, roverManifestData, roverPhotos, roverName, date, pages, page, perPage } = getState()
 
-    const inputDate = dayjs(date).format('YYYY-MM-DD')
+    const inputDate = dayjs( date ).format( 'YYYY-MM-DD' )
 
     return `
         <main>
             <h1>Welcome!</h1>
             <p>Please select a rover and date to view rover photos from that period.</p>
             <section id="menu">
-                <nav class="tabs">
-                    <ul>
-                        ${navigation()}
-                    </ul>
-                </nav>
-                <div class="hamburger">
+                <label for="hamburger-check" class="hamburger">
                     <span class="bar"></span>
                     <span class="bar"></span>
                     <span class="bar"></span>
-                </div>
-                <div class="date-selector">
-                    <input type="date" id="date" value="${inputDate}" />
-                    <button id="change-date">Update</button>
+                </label>
+                <input type="checkbox" id="hamburger-check">
+                <div class="nav-container">
+                    <nav class="tabs">
+                        <ul>
+                            ${navigation()}
+                        </ul>
+                    </nav>
+                    <div class="date-selector">
+                        <input type="date" id="date" value="${inputDate}" />
+                        <button id="change-date">Update</button>
+                    </div>
                 </div>
             </section>
             <section>
-                ${roverManifest(roverManifestData, roverName)}
+                ${roverManifest( roverManifestData, roverName )}
             </section>
             <section>
-                ${renderPhotos(roverPhotos, roverName, pages, page, perPage)}
+                ${renderPhotos( roverPhotos, roverName, pages, page, perPage )}
             </section>
         </main>
     `
 }
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root)
-})
+window.addEventListener( 'load', () => {
+    render( root )
+} )
 
-window.addEventListener('click', (event) => {
-    if( !event.target.classList.contains('rover-link') ){
+window.addEventListener( 'click', ( event ) => {
+    if( !event.target.classList.contains( 'rover-link' ) ){
         return
     }
 
     event.preventDefault()
 
-    const roverName = event.target.hash.replace(/^#+/,"");
+    const roverName = event.target.hash.replace( /^#+/,'' )
 
-    const date = document.getElementById('date').value
+    const date = document.getElementById( 'date' ).value
 
-    updateStore({roverName:roverName, roverManifestData: null, roverPhotos: null, date: date})
-})
+    updateStore( {roverName:roverName, roverManifestData: null, roverPhotos: null, date: date} )
+} )
 
-window.addEventListener('click', (event) => {
-    if(event.target != document.getElementById('change-date')){
+window.addEventListener( 'click', ( event ) => {
+    if( event.target != document.getElementById( 'change-date' ) ){
         return
     }
 
     event.preventDefault()
 
-    const date = document.getElementById('date').value
+    const date = document.getElementById( 'date' ).value
 
-    updateStore({roverPhotos: null, date: date})
-})
+    updateStore( {roverPhotos: null, date: date} )
+} )
 
 // ------------------------------------------------------  COMPONENTS
 
@@ -118,95 +122,95 @@ window.addEventListener('click', (event) => {
 const navigation = () => {
     let {rovers} = getState()
 
-    return rovers.map(name => {
+    return rovers.map( name => {
         return `<li><a href="#${name}" class="rover-link">${name}</a></li>`
-    }).join('')
+    } ).join( '' )
 }
 
 // Method to render rover manifest data
-const roverManifest = (roverManifestData, roverName) => {
-    if (!roverManifestData) {
-        getRoverManifest(getState(), roverName)
-        return (`
+const roverManifest = ( roverManifestData, roverName ) => {
+    if ( !roverManifestData ) {
+        getRoverManifest( getState(), roverName )
+        return ( `
         <div class="spinner">
             <div class="bounce1"></div>
             <div class="bounce2"></div>
             <div class="bounce3"></div>
         </div>
-        `)
+        ` )
     }
 
-    return (`
+    return ( `
         <p>This is the ${roverManifestData.name}. It was launched on ${roverManifestData.launch_date} and landed on Mars on
         ${roverManifestData.landing_date}. It's current status is ${roverManifestData.status}.</p>
-    `)
+    ` )
 }
 
 // Method to render rover photos
 // @todo fix photo rendering so empty string isn't returned first
-const renderPhotos = (roverPhotos, roverName, pages, page, perPage) => {
-    if(roverPhotos && 'undefined' !== typeof roverPhotos.error){
+const renderPhotos = ( roverPhotos, roverName, pages, page, perPage ) => {
+    if( roverPhotos && 'undefined' !== typeof roverPhotos.error ){
         return `<p>${roverPhotos.error}</p>`
     }
 
-    if(!roverPhotos) {
-        getRoverPhotos(getState(), roverName)
-        return (`
+    if( !roverPhotos ) {
+        getRoverPhotos( getState(), roverName )
+        return ( `
             <div class="spinner">
                 <div class="bounce1"></div>
                 <div class="bounce2"></div>
                 <div class="bounce3"></div>
             </div>
-        `)
+        ` )
     }
 
     const offset = perPage * page
 
-    const imageHTML = roverPhotos.map((photo, index) => {
+    const imageHTML = roverPhotos.map( ( photo, index ) => {
 
-        if((offset + perPage) <= index || index < offset){
+        if( ( offset + perPage ) <= index || index < offset ){
             return ''
         }
-        return (`
+        return ( `
         <div class="gallery-item">
                 <img src="${photo}" />
         </div>
-        `)
-    }).join('')
+        ` )
+    } ).join( '' )
 
-    return (`
+    return ( `
     <div class="gallery">
         <main id="image-gallery" class="images">
         ${imageHTML}
         </main>
-        ${renderNavigation(pages, page)}
+        ${renderNavigation( pages, page )}
     </div>
-    `)
+    ` )
 }
 
 // Set up gallery pagination/navigation
 const renderNavigation = () => {
-    return (`
+    return ( `
         <footer id="gallery-pagination">
             <button class="btnPrevious">&larr; <span class="sr-only">Previous</span></button>
             <button class="btnNext"><span class="sr-only">Next </span>&rarr;</button>
         </footer>
-    `)
+    ` )
 }
 
 // Add event listeners for navigation
-window.addEventListener('click', (event) => {
-    if(!event.target.classList.contains('gallery-dot')){
+window.addEventListener( 'click', ( event ) => {
+    if( !event.target.classList.contains( 'gallery-dot' ) ){
         return
     }
 
-    const page = parseInt(event.target.getAttribute('data-index'), 10)
+    const page = parseInt( event.target.getAttribute( 'data-index' ), 10 )
 
-    updateStore({page})
-})
+    updateStore( {page} )
+} )
 
-window.addEventListener('keydown', (event) => {
-    if(event.code !== 'ArrowLeft' && event.code !== 'ArrowRight'){
+window.addEventListener( 'keydown', ( event ) => {
+    if( event.code !== 'ArrowLeft' && event.code !== 'ArrowRight' ){
         return
     }
 
@@ -214,24 +218,24 @@ window.addEventListener('keydown', (event) => {
 
     let nextPage
 
-    if(event.code === 'ArrowLeft'){
+    if( event.code === 'ArrowLeft' ){
         nextPage = page - 1
-    } else if(event.code === 'ArrowRight'){
+    } else if( event.code === 'ArrowRight' ){
         nextPage = page + 1
     }
 
     page = nextPage
 
-    if(page < 0 || page >= pages){
+    if( page < 0 || page >= pages ){
         return
     }
 
-    updateStore({page})
-})
+    updateStore( {page} )
+} )
 
 // Previous Button
-window.addEventListener('click', (event) => {
-    if(!event.target.classList.contains('btnPrevious')){
+window.addEventListener( 'click', ( event ) => {
+    if( !event.target.classList.contains( 'btnPrevious' ) ){
         return
     }
 
@@ -241,16 +245,16 @@ window.addEventListener('click', (event) => {
 
     page = previousPage
 
-    if(page < 0){
+    if( page < 0 ){
         return
     }
 
-    updateStore({page})
-})
+    updateStore( {page} )
+} )
 
 // Next Button
-window.addEventListener('click', (event) => {
-    if(!event.target.classList.contains('btnNext')){
+window.addEventListener( 'click', ( event ) => {
+    if( !event.target.classList.contains( 'btnNext' ) ){
         return
     }
 
@@ -260,98 +264,40 @@ window.addEventListener('click', (event) => {
 
     page = nextPage
 
-    if(page >= pages){
+    if( page >= pages ){
         return
     }
 
-    updateStore({page})
-})
-
-// Load images
-function showImages(roverPhotos, roverName) {
-    const gallery = document.getElementById('image-gallery')
-    let { perPage, page, pages } = getState()
-
-    if(gallery){
-        while(gallery.firstChild){
-            gallery.removeChild(gallery.firstChild)
-        }
-
-        var offset = (page - 1) * perPage;
-        var dots = document.querySelectorAll('.gallery-dot');
-
-        for (var i = 0; i < dots.length; i++){
-            dots[i].classList.remove('active');
-        }
-
-        dots[page - 1].classList.add('active');
-    }
-
-    for (var i = offset; i < offset + perPage; i++) {
-        if ( images[i] ) {
-            var template = document.createElement('div');
-            var title = document.createElement('p');
-            var titleText = document.createTextNode(images[i].title);
-            var img = document.createElement('img');
-
-            template.classList.add('template')
-            img.setAttribute("src", images[i].source);
-            img.setAttribute('alt', images[i].title);
-
-            title.appendChild(titleText);
-            template.appendChild(img);
-            template.appendChild(title);
-            gallery.appendChild(template);
-        }
-    }
-
-    const animateItemIn = (i) => {
-        var item = galleryItems[i];
-        return () => item.classList.add('animate');
-    }
-
-    // Animate images
-    var galleryItems = document.querySelectorAll('.template')
-    for (var i = 0; i < galleryItems.length; i++) {
-        var onAnimateItemIn = animateItemIn(i);
-        setTimeout(onAnimateItemIn, i * 100);
-    }
-
-    // Update page indicator
-    const pageIndicator = document.getElementById('page')
-
-    pageIndicator.textContent = "Page " + page + " of " + pages;
-
-    gallerySetUp()
-}
+    updateStore( {page} )
+} )
 
 // ------------------------------------------------------  API CALLS
 
 // API call for rover manifest data
-const getRoverManifest = (state, roverName) => {
+const getRoverManifest = ( state, roverName ) => {
     let {roverManifestData} = state
 
-    fetch(`http://localhost:3000/rovers/${roverName}`)
-        .then(res => res.json())
-        .then(roverManifestData => updateStore({roverManifestData}))
+    fetch( `http://localhost:3000/rovers/${roverName}` )
+        .then( res => res.json() )
+        .then( roverManifestData => updateStore( {roverManifestData} ) )
 
     return roverManifestData
 }
 
 // API call to get photos per rover
-const getRoverPhotos = (state, roverName) => {
+const getRoverPhotos = ( state, roverName ) => {
     let {roverPhotos, date, perPage } = state
 
     const url = `http://localhost:3000/mars-photos/${roverName}/${date}`
 
-    fetch(url)
-        .then(res => res.json())
-        .then(roverPhotos => {
+    fetch( url )
+        .then( res => res.json() )
+        .then( roverPhotos => {
             const page = 0
-            const pages = Math.ceil(roverPhotos.length / perPage)
+            const pages = Math.ceil( roverPhotos.length / perPage )
 
-            return updateStore({roverPhotos, page, pages})
-        })
+            return updateStore( {roverPhotos, page, pages} )
+        } )
 
     return roverPhotos
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,45 +1,45 @@
-require('dotenv').config()
-const express = require('express')
-const { Map, List } = require('immutable');
-const dayjs = require('dayjs');
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path');
-const res = require('express/lib/response');
+require( 'dotenv' ).config()
+const express = require( 'express' )
+const { Map, List } = require( 'immutable' )
+const dayjs = require( 'dayjs' )
+const bodyParser = require( 'body-parser' )
+const fetch = require( 'node-fetch' )
+const path = require( 'path' )
+const res = require( 'express/lib/response' )
 
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use( '/', express.static( path.join( __dirname, '../public' ) ) )
 
 // your API calls
-app.get('favicon.ico', async(req, res) => {
+app.get( 'favicon.ico', async( req, res ) => {
     return ''
-})
+} )
 
 //Get Mars rover manifest data
-app.get('/rovers/:roverName', async(req, res, next) => {
+app.get( '/rovers/:roverName', async( req, res, next ) => {
     const roverName = req.params.roverName
 
     if( !roverName ) {
-        res.status(404)
-        res.render('error', { error: 'Rover not found' })
+        res.status( 404 )
+        res.render( 'error', { error: 'Rover not found' } )
     }
 
     try {
-        let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}/?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
+        let roverData = await fetch( `https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}/?api_key=${process.env.API_KEY}` )
+            .then( res => res.json() )
 
-            .then(res => {
-                console.log(res)
+            .then( res => {
+                console.log( res )
                 return res
-            })
+            } )
 
-            .then(res => {
-                if(res.errors){
+            .then( res => {
+                if( res.errors ){
                     return res.errors
                 }
 
@@ -47,52 +47,52 @@ app.get('/rovers/:roverName', async(req, res, next) => {
 
                 return {
                     name: rover.name,
-                    landing_date: dayjs(rover.landing_date).format('MMMM D, YYYY'),
-                    launch_date: dayjs(rover.launch_date).format('MMMM D, YYYY'),
+                    landing_date: dayjs( rover.landing_date ).format( 'MMMM D, YYYY' ),
+                    launch_date: dayjs( rover.launch_date ).format( 'MMMM D, YYYY' ),
                     status: rover.status,
                     max_date: rover.max_date,
                 }
-            })
+            } )
 
-        res.send(roverData)
-    } catch(err) {
-        console.log('error:', err);
+        res.send( roverData )
+    } catch( err ) {
+        console.log( 'error:', err )
     }
-})
+} )
 
 //Get Mars rover photos
-app.get('/mars-photos/:roverName/:filterDate', async(req,res) => {
+app.get( '/mars-photos/:roverName/:filterDate', async( req,res ) => {
     const roverName = req.params.roverName
 
-    const filterDate = dayjs( req.params.filterDate ).format( 'YYYY-M-D' );
+    const filterDate = dayjs( req.params.filterDate ).format( 'YYYY-M-D' )
 
     try {
-        let roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${filterDate}&api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
+        let roverPhotos = await fetch( `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${filterDate}&api_key=${process.env.API_KEY}` )
+            .then( res => res.json() )
 
-            .then(res => {
-                console.log(res)
+            .then( res => {
+                console.log( res )
                 return res
-            })
+            } )
 
-            .then(res => res.photos)
+            .then( res => res.photos )
 
-            .then(res => {
-                if(!res.length){
-                    return {error: 'Whoops, no pictures today!'}
+            .then( res => {
+                if( !res.length ){
+                    return { error: 'Whoops, no pictures today!' }
                 }
 
-                const roverPhotos = List(res)
+                const roverPhotos = List( res )
 
-                return roverPhotos.map((photo, key) => {
+                return roverPhotos.map( ( photo, key ) => {
                     return photo.img_src
-                })
-            })
+                } )
+            } )
 
-            res.send(roverPhotos)
-    } catch(err) {
-        console.log('error:', err);
+            res.send( roverPhotos )
+    } catch( err ) {
+        console.log( 'error:', err )
     }
-})
+} )
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen( port, () => console.log( `Example app listening on port ${port}!` ) )
